dd75e7b147aef6aa01d8bd9651261a6ecbb66965
tideYHcode <113647991+tideYH@users.noreply.github.com>
2024-03-26 23:50:50 +0800

    第四次作业提交

---
+++ assignment4/homework41/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    // 泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intList = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intList.Add(x);
+            }
+
+            // 使用ForEach打印所有元素
+            intList.ForEach(x => Console.WriteLine(x));
+
+            // 求最大值
+            int max = int.MinValue;
+            intList.ForEach(x => {
+                if (x > max) max = x;
+            });
+            Console.WriteLine($"最大值: {max}");
+
+            // 求最小值
+            int min = int.MaxValue;
+            intList.ForEach(x => {
+                if (x < min) min = x;
+            });
+            Console.WriteLine($"最小值: {min}");
+
+            // 求和
+            int sum = 0;
+            intList.ForEach(x => sum += x);
+            Console.WriteLine($"总和: {sum}");
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+
+            // 使用ForEach打印所有字符串元素
+            strList.ForEach(s => Console.WriteLine(s));
+
+            // 可以根据需要对字符串进行其他操作，比如打印长度等
+        }
+    }
+}
---
+++ assignment4/homework42/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace AlarmClock
+{
+    // 定义闹钟类
+    public class AlarmClock
+    {
+        // 事件：嘀嗒
+        public event EventHandler Tick;
+
+        // 事件：响铃
+        public event EventHandler Alarm;
+
+        // 设置闹钟时间
+        public TimeSpan AlarmTime { get; set; }
+
+        // 启动闹钟
+        public void Start()
+        {
+            while (true)
+            {
+                // 获取当前时间
+                var currentTime = DateTime.Now.TimeOfDay;
+
+                // 触发嘀嗒事件
+                Tick?.Invoke(this, EventArgs.Empty);
+
+                // 判断是否到达闹钟时间
+                if (currentTime >= AlarmTime)
+                {
+                    // 触发响铃事件
+                    Alarm?.Invoke(this, EventArgs.Empty);
+                    break;
+                }
+
+                // 等待一秒钟
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var alarmClock = new AlarmClock();
+            alarmClock.AlarmTime = new TimeSpan(8, 0, 0); // 设置闹钟时间为早上8点
+
+            // 订阅嘀嗒事件
+            alarmClock.Tick += (sender, e) =>
+            {
+                Console.WriteLine("嘀嗒...");
+            };
+
+            // 订阅响铃事件
+            alarmClock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("闹钟响了！该起床了！");
+            };
+
+            // 启动闹钟
+            alarmClock.Start();
+        }
+    }
+}
